541950d692be22ce82fe8fa4edbd04fd97f0b583
Adeeshanethsara190 <111955383+Adeeshanethsara190@users.noreply.github.com>
2022-08-24 13:34:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,19 +21,19 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94763427338']
-global.premium = ['94763427338']
-global.ownernomer = '94763427338'
-global.ownername = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
-global.botname = 'VIHANGA MD BOT'
-global.footer = 'VIHANGA MD'
-global.cap = '*Genarated By Vihanga Md*'
+global.owner = ['94728873207']
+global.premium = ['94728873207']
+global.ownernomer = '94728873207'
+global.ownername = 'NETHSARA  MD INF BOT〽️'
+global.botname = 'NETHSARA MD BOT'
+global.footer = 'NETHSARA MD'
+global.cap = '*Genarated By Nethsara Md*'
 global.ig = 'https://github.com/vihangabot'
 global.region = 'Sri Lanka, Sabaragamuwa, Embilipitiya'
 global.sc = 'https://github.com/vihangabot'
 global.myweb = 'https://chat.whatsapp.com/BDge3i6chrQKWkkSRellyT'
-global.packname = 'Vihanga Md Sticker Bot'
-global.author = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
+global.packname = 'NETHSARA Md Sticker Bot'
+global.author = 'NETHSARA MD INF BOT〽️'
 global.autobio = 'true'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
